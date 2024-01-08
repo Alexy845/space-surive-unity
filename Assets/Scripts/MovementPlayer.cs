4485a735a5a5e8df49bae8b9f09912233e086f53
MartiFiak <jacolotmarine@gmail.com>
2024-01-08 17:43:32 +0100

    Border collision[OK]

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -15,6 +15,7 @@ public class MovementPlayer : MonoBehaviour
         moveY = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveX, moveY);
         transform.Translate(movement * Time.deltaTime * speed);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -19f, 22f), Mathf.Clamp(transform.position.y, -8f, 9f), transform.position.z);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
