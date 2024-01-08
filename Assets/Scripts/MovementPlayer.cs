4289340b1ad5c1f391cb04e074f4436e1e4cca86
MartiFiak <jacolotmarine@gmail.com>
2024-01-08 14:31:40 +0100

    MovementPlayer[OK]

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -7,15 +7,12 @@ public class MovementPlayer : MonoBehaviour
     public float moveX = 0;
     public float moveY = 0;
     public float speed = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveX, moveY);
         transform.Translate(movement * Time.deltaTime * speed);
     }
